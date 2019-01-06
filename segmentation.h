fd8e190e41a55be4559c3bd5e6e8f4b248fca672
Camillemns <43649864+Camillemns@users.noreply.github.com>
2019-01-06 22:12:59 +0100

    Delete segmentation.h

--- segmentation.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEGMENTATION_H
-# define SEGMENTATION_H
-
-void horizontal(SDL_Surface *img);
-void vertical(SDL_Surface *img, int starter , int ending);
-void separate(SDL_Surface *img);
-
-#endif /* !SEGMENTATION_H */
