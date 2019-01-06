90255cc496ba247a4171f90fa70cbe3aaf5723d9
Camillemns <43649864+Camillemns@users.noreply.github.com>
2019-01-06 22:11:13 +0100

    Delete dataprep.h

--- dataprep.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void free_train(float **train);
-void free_test(float **test);
-int put_train_value_in_array(float **train, int *label_train);
-int put_test_value_in_array(float **test, int *label_test);
